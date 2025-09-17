5338d1b00c877f7dd2b1060687c5c0a7a9d07f8f
no name <your-email@example.com>
2025-09-17 21:58:17 +0530

    welcomepage is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "./components/Login";
+import WelcomePage from "./components/WelcomePage";
 
 const App = () => {
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-gray-200">
-      <Login />
+      {/* <Login /> */}
+      <WelcomePage/>
     </div>
   );
 };
