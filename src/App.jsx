3c4212105fd0c2c573d56364e8fd42258c0bacce
no name <your-email@example.com>
2025-09-18 20:40:47 +0530

    Myaccount page is added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Login from "./components/Login";
 import WelcomePage from "./components/WelcomePage";
 import Signup from "./components/Signup";
+import MyAccount from "./components/MyAccount";
 
 const App = () => {
   return (
     <div className="flex items-center justify-center h-screen w-screen bg-gray-200">
       {/* <Login /> */}
       {/* <WelcomePage/> */}
-      <Signup />
+      {/* <Signup /> */}
+      <MyAccount />
     </div>
   );
 };
