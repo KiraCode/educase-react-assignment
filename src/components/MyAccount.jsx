8292371d2283ad5d32991f5a1f333978a2a16e81
no name <your-email@example.com>
2025-09-18 20:41:23 +0530

    my account page is added

---
+++ src/components/MyAccount.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import profile from "../assets/profile.png";
+import camera from "../assets/camera.png";
+
+const MyAccount = () => {
+  return (
+    <div className="aspect-[9/16] h-full flex flex-col items-start justify-start bg-[#f7f8f9]">
+      <div className="w-full font-semibold text-xl py-6 shadow-xs bg-white p-4">
+        Account Settings
+      </div>
+      <div className="account border-b text-sm border-b-gray-400 border-dashed p-4">
+        <div className="flex gap-2 py-4">
+          <div className="relative">
+            <img src={profile} alt="Profile" className="w-16 h-16 rounded-full mr-4" />
+            <div className="absolute bottom-0 rounded-full p-1 bg-[#6c25ff] right-2">
+              <img src={camera} alt="Camera" className="w-3 invert h-3" />
+            </div>
+          </div>
+          <div>
+            <p className="font-semibold">user</p>
+            <p className="text-gray-600 text-xs">user@gmail.com</p>
+          </div>
+        </div>
+        <p className="font-semibold text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque,
+          labore! Quae sequi nobis ratione magni cupiditate animi?
+          Exercitationem, quibusdam explicabo?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default MyAccount;
